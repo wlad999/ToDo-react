a3719f1bd4493c62fa2ad468ec0a63e196d0e3e1
wlad999 <wladmail25@gmeil.com>
2020-06-08 12:51:19 +0300

    added structure

--- src/index.js
+++ src/index.js
@@ -1,29 +1,6 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-import AppHeader from "./components/AppHeader";
-import SearchPanel from "./components/SearchPanel";
-import TodoList from "./components/TodoList";
-import ItemStatusFilter from "./components/ItemStatusFilter";
+import App from "./components/App";
 
-import "./index.css";
-
-const App = () => {
-  const todoData = [
-    { label: "Drink Coffee", important: false, id: 1 },
-    { label: "Make Awesome App", important: true, id: 2 },
-    { label: "Have a lunch", important: false, id: 3 },
-  ];
-
-  return (
-    <div className="todo-app">
-      <AppHeader toDo={1} done={3} />
-      <div className="top-panel d-flex">
-        <SearchPanel />
-        <ItemStatusFilter />
-      </div>
-      <TodoList todos={todoData} />
-    </div>
-  );
-};
 ReactDom.render(<App />, document.getElementById("root"));
