b7f5ef9c43f366353d00bba7b040b144a8cc7438
wlad999 <wladmail25@gmeil.com>
2020-06-08 10:37:45 +0300

    before activity

---
+++ src/components/SearchPanel.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./SearchPanel.css";
+
+const SearchPanel = () => {
+  return (
+    <input
+      type="text"
+      className="form-control search-input"
+      placeholder="type to search"
+    />
+  );
+};
+
+export default SearchPanel;
